01a2e083bed971a46b58c0e9b28d83ffd74fdd38
Akiva Gubbay <gubbayakiva@gmail.com>
2019-01-13 19:12:59 +0200

    bug fixes

--- src/containers/hot-folders-page.js
+++ src/containers/hot-folders-page.js
@@ -36,7 +36,7 @@ class HotFoldersPage extends React.Component {
   componentWillMount() {
     //if (!this.props.user.login)
     //  this.props.history.push('/login');
-    //this.getPrintCodes(this.state.paging.current+1, this.state.pagesize);
+    this.getPrintCodes(this.state.paging.current + 1, this.state.pagesize);
   }
 
   handlePageChanged(page) {
